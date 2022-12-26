1d63e4fee26ccd5986b3606b3a1cfac1ca5e9d89
joaoCabralpv <joaocabralpv@gmail.com>
2022-12-26 16:11:11 -0100

    Auto call functions

--- pyCLM.py
+++ pyCLM.py
@@ -6,13 +6,23 @@ class Inside:
 #foo2 test
 #foo3 word
 
-def menu(choises , *, index="", indexStart=1, Input=False):
+def menu(choises , *, index="", indexStart=1, Input=False, functions=[],functionIndexStart=None):
     #loops trough the choises
+    if functionIndexStart==None:
+        functionIndexStart=indexStart
     for i in range(len(choises)):
         if index:
             #Adds the index to the choise. $Number is replaced with the number of the index startig at indexSart
             print(index.replace("$Number",str(i+indexStart)) + choises[i])
         else:
-            print(choises)
+            print(choises[i])
     if Input:
-        return input()
+        InputResult=input()
+        if not InputResult.isnumeric():
+            #Makes so that the functions arent called, avoinding a crash
+            functions=False
+        if functions:
+            functions[int(InputResult)-functionIndexStart]()
+            return InputResult
+
+
