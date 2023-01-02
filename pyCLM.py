050464de83bdf43a487dcc8601f9470eb6666854
João Cabral <joaocabralpv@gmail.com>
2023-01-02 16:45:06 -0100

    index skip

--- pyCLM.py
+++ pyCLM.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 class Inside:
     pass
 
@@ -6,23 +8,25 @@ class Inside:
 #foo2 test
 #foo3 word
 
-def menu(choises , *, index="", indexStart=1, Input=False, functions=[],functionIndexStart=None):
+def menu(choises , *, index="", indexStart=1, indexSkip=0 ,Input=False,functions=[],functionIndexStart=None):
     #loops trough the choises
+    indexSkip+=1
     if functionIndexStart==None:
         functionIndexStart=indexStart
     for i in range(len(choises)):
-        if index:
-            #Adds the index to the choise. $Number is replaced with the number of the index startig at indexSart
-            print(index.replace("$Number",str(i+indexStart)) + choises[i])
-        else:
-            print(choises[i])
+            #if index:
+                #Adds the index to the choise. $Number is replaced with the number of the index startig at indexSart
+            print(index.replace("$Number",str((i*indexSkip)+indexStart)) + choises[i])
+            #else:
+             #   print(choises[i])
     if Input:
         InputResult=input()
         if not InputResult.isnumeric():
             #Makes so that the functions arent called, avoinding a crash
             functions=False
         if functions:
-            functions[int(InputResult)-functionIndexStart]()
+            functions[ceil(int(InputResult))-functionIndexStart]()
             return InputResult
 
 
+menu(["a","b","c","d"],index="$Number", indexSkip=1)
